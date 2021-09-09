dd783435e1e99afcc5b6749738356ff2dc1f846f
gnivler <gnivil@gmail.com>
2021-09-09 16:03:16 -0600

    1.1

--- Mod.cs
+++ Mod.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using BepInEx;
 using HarmonyLib;
 
@@ -9,18 +11,46 @@ namespace StoreSellPrices
     {
         private const string PluginGUID = "ca.gnivler.d2e.StoreSellPrices";
         private const string PluginName = "StoreSellPrices";
-        private const string PluginVersion = "1.0.0";
+        private const string PluginVersion = "1.1.0";
 
         private void Awake()
         {
             Harmony harmony = new("ca.gnivler.d2e.StoreSellPrices");
-            harmony.PatchAll();
-            //Log("StoreSellPrices Startup");
+            harmony.PatchAll(typeof(Patches));
+            Log("StoreSellPrices Startup");
         }
 
         internal static void Log(object input)
         {
-            File.AppendAllText("log.txt",$"{input ?? "null"}\n");
+            //File.AppendAllText("log.txt",$"{input ?? "null"}\n");
+        }
+        
+        internal static void PrintInstructionsAroundInsertion(List<CodeInstruction> codes, int insertPoint, int insertSize, int adjacentNum = 5)
+        {
+            Log($"Inserting {insertSize} at {insertPoint}.");
+
+            // in case insertPoint is near the start of the method's IL
+            var adjustedAdjacent = codes.Count - adjacentNum >= 0 ? adjacentNum : Math.Max(0, codes.Count - adjacentNum);
+            for (var i = 0; i < adjustedAdjacent; i++)
+            {
+                // codes[266 - 5 + 0].opcode
+                // codes[266 - 5 + 4].opcode
+                Log($"{codes[insertPoint - adjustedAdjacent + i].opcode,-10}{codes[insertPoint - adjustedAdjacent + i].operand}");
+            }
+
+            for (var i = 0; i < insertSize; i++)
+            {
+                Log($"{codes[insertPoint + i].opcode,-10}{codes[insertPoint + i].operand}");
+            }
+
+            // in case insertPoint is near the end of the method's IL
+            adjustedAdjacent = insertPoint + adjacentNum <= codes.Count ? adjacentNum : Math.Max(codes.Count, adjustedAdjacent);
+            for (var i = 0; i < adjustedAdjacent; i++)
+            {
+                // 266 + 2 - 5 + 0
+                // 266 + 2 - 5 + 4
+                Log($"{codes[insertPoint + insertSize + adjustedAdjacent + i].opcode,-10}{codes[insertPoint + insertSize + adjustedAdjacent + i].operand}");
+            }
         }
     }
 }
--- Patches.cs
+++ Patches.cs
@@ -1,36 +1,154 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
 using Devdog.General;
+using Devdog.InventoryPro;
 using HarmonyLib;
-using Tiled2Unity;
 using static StoreSellPrices.Mod;
+using Object = UnityEngine.Object;
+
+// ReSharper disable InconsistentNaming
 
 namespace StoreSellPrices
 {
     public static class Patches
     {
-        [HarmonyPatch(typeof(TiggerStop), "Enter")]
-        public static class TiggerStopEnterPatch
+        // rewrite
+        [HarmonyPatch(typeof(InfoBoxUI), "SellTip")]
+        public static bool Prefix(InfoBoxUI __instance, InventoryItemBase ___currentItem, ref bool __runOriginal)
         {
-            public static void Postfix(TiggerStop __instance)
+            __runOriginal = false;
+            // resize array of 3 into 6
+            if (__instance.m_SellInfoObj.Length == 3)
             {
-                Town.CurCity = __instance;
-                foreach (var inventoryItemBase in __instance.container.items)
+                Array.Resize(ref __instance.m_SellInfoObj, 6);
+                for (var i = 0; i < 3; i++)
                 {
-                    UnitySingleton<BigTable>.Instance.ResetPrice(inventoryItemBase);
-                    UnitySingleton<BigTable>.Instance.AddItemSellInfo(inventoryItemBase.MyID, __instance.TownID, inventoryItemBase.sellPrice.amount);
+                    __instance.m_SellInfoObj[3 + i] = Object.Instantiate(__instance.m_SellInfoObj[0], __instance.m_SellObj, false);
+                    __instance.m_SellInfoObj[3 + i].gameObject.SetActive(false);
+                }
+            }
+
+            foreach (var commonObj in __instance.m_SellInfoObj)
+            {
+                commonObj.gameObject.SetActive(false);
+            }
+
+            var myID = ___currentItem.MyID;
+            var itemSellInfo = UnitySingleton<BigTable>.Instance.m_ItemSellInfo;
+            if (itemSellInfo.TryGetValue(myID, out var itemTradeInfos))
+            {
+                itemTradeInfos = itemTradeInfos.OrderBy(i => i.m_Price).ToList();
+                for (var i = 0; i < __instance.m_SellInfoObj.Length && i < itemTradeInfos.Count; i++)
+                {
+                    var tradeInfo = itemTradeInfos[i];
+                    Log($"{___currentItem.name} {__instance.m_SellInfoObj[i].m_Text[0].text,-25} {__instance.m_SellInfoObj[i].m_Text[1].text}");
+                    __instance.m_SellInfoObj[i].gameObject.SetActive(true);
+                    __instance.m_SellInfoObj[i].m_Text[0].text = Town.m_Cities[tradeInfo.m_CityID].TownName;
+                    __instance.m_SellInfoObj[i].m_Text[1].text = GlobalManager.FormatGoldValue(tradeInfo.m_Price);
+                }
+            }
+            else
+            {
+                __instance.m_SellInfoObj[0].m_Text[0].text = LanguageManager.Instance.GetKey("NoTradeInfo");
+                __instance.m_SellInfoObj[0].m_Text[1].text = string.Empty;
+                __instance.m_SellInfoObj[0].gameObject.SetActive(value: true);
+            }
+
+            return false;
+        }
+
+        // TODO  add items to price storage when crafting 
+        // rewrite - maybe keeps more than 3 entries
+        [HarmonyPatch(typeof(BigTable), "AddItemSellInfo")]
+        public static bool Prefix(BigTable __instance, string itemid, string cityid, float price, ref bool __runOriginal)
+        {
+            __runOriginal = false;
+            if (!__instance.m_ItemSellInfo.ContainsKey(itemid))
+            {
+                var itemTradeInfo = new ItemTradeInfo();
+                itemTradeInfo.m_CityID = cityid;
+                itemTradeInfo.m_Price = price;
+                __instance.m_ItemSellInfo[itemid] = new List<ItemTradeInfo>();
+                __instance.m_ItemSellInfo[itemid].Add(itemTradeInfo);
+                return false;
+            }
+
+            var num = 0;
+            while (num < __instance.m_ItemSellInfo[itemid].Count)
+            {
+                if (__instance.m_ItemSellInfo[itemid][num] == null)
+                {
+                    __instance.m_ItemSellInfo[itemid].RemoveAt(num);
+                    continue;
                 }
 
-                var collection = PlayerManager.instance.currentPlayer.inventoryPlayer.inventoryCollections[0];
-                foreach (var collectionItem in collection)
+                if (__instance.m_ItemSellInfo[itemid][num].m_CityID == cityid)
                 {
-                    if (collectionItem.item is null
-                        || !collectionItem.item.isSellable)
-                    {
-                        continue;
-                    }
-
-                    UnitySingleton<BigTable>.Instance.ResetPrice(collectionItem.item);
-                    UnitySingleton<BigTable>.Instance.AddItemSellInfo(collectionItem.item.MyID, __instance.TownID, collectionItem.item.sellPrice.amount);
+                    __instance.m_ItemSellInfo[itemid].RemoveAt(num);
+                    break;
                 }
+
+                num++;
+            }
+
+            var itemTradeInfo2 = new ItemTradeInfo();
+            itemTradeInfo2.m_CityID = cityid;
+            itemTradeInfo2.m_Price = price;
+            __instance.m_ItemSellInfo[itemid].Add(itemTradeInfo2);
+            return false;
+        }
+
+        // resize limited List
+        [HarmonyPatch(typeof(InventoryItemBase), "GetInfo")]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            // strategy
+            // change new List<>(3) into an unspecified size new List<>(), so it's a different ctor
+            var codes = new List<CodeInstruction>(instructions);
+            var intCtor = AccessTools.Constructor(typeof(List<ItemInfoRow>), new[] { typeof(int) });
+            var defaultCtor = AccessTools.Constructor(typeof(List<ItemInfoRow>), new Type[] { });
+
+            for (var index = 0; index < codes.Count; index++)
+            {
+                var code = codes[index];
+                if (code.opcode == OpCodes.Ldc_I4_3
+                    && ReferenceEquals(codes[index + 1]?.operand, intCtor))
+                {
+                    Log("InventoryItemBaseGetInfoTranspiler");
+                    code.opcode = OpCodes.Nop;
+                    codes[index + 1].operand = defaultCtor;
+                    break;
+                }
+            }
+
+            return codes.AsEnumerable();
+        }
+
+        // populate DB with sell info
+        [HarmonyPatch(typeof(TiggerStop), "Enter")]
+        public static void Postfix(TiggerStop __instance)
+        {
+            Town.CurCity = __instance;
+            AccessTools.Method(typeof(TiggerStop), "RefreshItemPrice").Invoke(__instance, new object[] { });
+            foreach (var inventoryItemBase in __instance.container.items)
+            {
+                UnitySingleton<BigTable>.Instance.ResetPrice(inventoryItemBase);
+                UnitySingleton<BigTable>.Instance.AddItemSellInfo(inventoryItemBase.MyID, __instance.TownID, inventoryItemBase.sellPrice.amount);
+            }
+
+            var collection = PlayerManager.instance.currentPlayer.inventoryPlayer.inventoryCollections[0];
+            foreach (var collectionItem in collection)
+            {
+                if (collectionItem.item is null
+                    || !collectionItem.item.isSellable)
+                {
+                    continue;
+                }
+
+                UnitySingleton<BigTable>.Instance.ResetPrice(collectionItem.item);
+                UnitySingleton<BigTable>.Instance.AddItemSellInfo(collectionItem.item.MyID, __instance.TownID, collectionItem.item.sellPrice.amount);
             }
         }
     }
